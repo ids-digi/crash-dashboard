c76f1a63b679ed94694343f75980361efd76a8e4
carsonology <carson.terbush@gmail.com>
2023-04-01 17:10:54 -0400

    create map context

--- src/Map.js
+++ src/Map.js
@@ -1,146 +1,96 @@
 import React, { useState, useEffect, useRef } from "react";
-import geojson from './data/master_crash_clean.geojson'
-import districts from './data/city-council-districts.geojson'
-import * as turf from '@turf/turf'
 import mapboxgl from 'mapbox-gl';
+import districts from './data/city-council-districts.geojson'
 import "mapbox-gl/dist/mapbox-gl.css";
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 function Map(props) {
-    const { hexVisibility, hexOpacity, dotSize, districtVisibility } = props
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY3RlcmJ1c2giLCJhIjoiY2t0dnZrYjM4MmU0aDJvbzM1dTFqbDY1NiJ9.zdZur9mZOlVhIxAoiqVwBA'
-
-    const [data, setData] = useState(geojson)
+    const { hexVisibility, districtVisibility, data, hexGridData } = props
 
     const [map, setMap] = useState(null);
     const mapContainer = useRef(null);
 
-    const bbox = [-86.70764843085207, 38.980672784175255, -86.33708588689486, 39.54695628365925]
-    const cellSide = .5;
-    const options = {};
-    const hexGridData = turf.hexGrid(bbox, cellSide, options);
-
-    // console.log(hexGridData)
+    const districtColor = "rgba(255,255,255,0.2)"
+    const hexColor = "rgb(119, 216, 240)"
+    const pointColor = "yellow"
+    const borderColor = "rgb(53, 53, 53)"
 
     useEffect(() => {
-        fetch(data)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => setData(data));
 
-    }, [])
-
-    useEffect(() => {
-        // mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
+        /* 
+            INITIALIZE MAP
+        */
         const initializeMap = ({ setMap, mapContainer }) => {
             const map = new mapboxgl.Map({
+                // it will know where to put the map based on the mapContainer ref
                 container: mapContainer.current,
-                style: "mapbox://styles/mapbox/dark-v10", // stylesheet location
+                // style: "mapbox://styles/mapbox/dark-v10",
+                style: "mapbox://styles/cterbush/clfyfv364003s01o4xuofdpp3",
+                // center it over Bloomington
                 center: [-86.52702437238956, 39.1656613635316],
                 zoom: 12,
+                // prevent from zooming out too far
                 minZoom: 10
-            }).fitBounds([
-                [-86.61974841955835, 39.111017400606066],
-                [-86.46310073817295, 39.21646913099789]
-            ]).addControl(
+            }).addControl(
+                // add geocoder to enable search
                 new MapboxGeocoder({
                     accessToken: mapboxgl.accessToken,
                     mapboxgl: mapboxgl
                 })
             )
 
-            hexGridData.features.map((hex) => {
-                hex.properties.numPoints = turf.within(data, hex).features.length
-                return hex
-            })
-
-            const binWMostPoints = hexGridData.features.reduce(
-                (prev, current) => {
-                    return prev.properties.numPoints > current.properties.numPoints ? prev : current
-                }
-            )
-
-            hexGridData.features.map((hex) => {
-                hex.properties.density = hex.properties.numPoints / binWMostPoints.properties.numPoints
-                return hex
-            })
-
-            // Create a popup, but don't add it to the map yet.
-            const popup = new mapboxgl.Popup({
-                closeButton: false,
-                closeOnClick: false
-            })
-
             map.on("load", () => {
                 setMap(map)
                 map.resize()
 
+                /*
+                    ADD DATA SOURCES
+                */
+                // crash data points 
                 map.addSource('crash-data-source', {
                     'type': 'geojson',
                     'data': data
                 })
-
+                // city council district areas 
                 map.addSource('council-districts', {
                     'type': 'geojson',
                     'data': districts
                 })
+                // hexbins geojson (generated in MapContext)
+                map.addSource('hexbin-data', {
+                    'type': 'geojson',
+                    'data': hexGridData
+                })
 
+                /*
+                    ADD LAYERS + STYLING
+                */
+                // city council districts
                 map.addLayer({
                     id: 'districts',
                     type: 'fill',
                     source: 'council-districts',
-                    // filter: ['!', ['has', 'point_count']],
                     paint: {
-                        'fill-color': 'rgba(255,255,255,0.1)',
+                        'fill-color': districtColor,
                         'fill-opacity': ['case',
                             ['boolean', ['feature-state', 'hover'], false],
                             0.8,
                             0.5
                         ]
-                        // 'fill-outline-color': 'white',
-                        // 'fill-outline-width': 3
                     },
-                })
-
-                map.addLayer({
-                    id: 'points',
-                    type: 'circle',
-                    source: 'crash-data-source',
-                    // filter: ['!', ['has', 'point_count']],
-                    paint: {
-                        'circle-color': "#3182bd",
-                        'circle-radius': ['interpolate', ['linear'], ['zoom'], 10, 1, 12, 1.5, 20, 20],
-                        'circle-stroke-width': 1,
-                        'circle-stroke-color': 'transparent',
-                        'circle-opacity': 0.3
-                    }
-                })
-
-                map.addLayer({
-                    id: 'hexBins',
-                    type: 'fill',
-                    source: {
-                        'type': 'geojson',
-                        'data': hexGridData
-                    },
-                    'layout': {
-                        'visibility': 'visible'
-                    },
+                }).addLayer({
+                    // gray border on each district
+                    'id': 'district-borders',
+                    'type': 'line',
+                    'source': 'council-districts',
+                    'layout': {},
                     'paint': {
-                        'fill-color': '#088',
-                        'fill-opacity': [
-                            "interpolate", ["linear"], ["get", "density"],
-                            // if there are zero points, opacity = 0
-                            0, 0,
-                            // if the density = 1, opacity = 60%
-                            1, .8
-                        ]
+                        'line-color': borderColor,
+                        'line-width': 2
                     }
-                })
-
-                map.addLayer({
+                }).addLayer({
+                    // label each district
                     'id': 'district-labels',
                     'type': 'symbol',
                     'source': 'council-districts',
@@ -148,28 +98,78 @@ function Map(props) {
                         'text-field': [
                             'format',
                             ['get', 'district_name'],
-                            { 'font-scale': 0.8 }
+                            { 'font-scale': 0.6 }
                         ],
-                        // 'text-font': ['Fira Sans', 'Arial Unicode MS Bold']
                     },
                     "paint": {
-                        "text-color": "#ffffff"
-                    }
-                });
-
-                map.addLayer({
-                    'id': 'district-borders',
-                    'type': 'line',
-                    'source': 'council-districts',
-                    'layout': {},
-                    'paint': {
-                        'line-color': 'gray',
-                        'line-width': 1
+                        "text-color": borderColor
                     }
                 })
 
-                let hoveredStateId = null;
+                // hexbins
+                map.addLayer({
+                    id: 'hexBins',
+                    type: 'fill',
+                    source: 'hexbin-data',
+                    // set visibility as visible initially
+                    // then it can be toggled later 
+                    'layout': {
+                        'visibility': 'visible'
+                    },
+                    'paint': {
+                        'fill-color': hexColor,
+                        'fill-opacity': [
+                            "interpolate", ["linear"], ["get", "density"],
+                            // if there are zero points, max opacity = 0
+                            0, 0,
+                            // if the density = 1, max opacity = 80%
+                            1, .8
+                        ]
+                    }
+                }).addLayer({
+                    // light border on each hexbin
+                    'id': 'hex-borders',
+                    'type': 'line',
+                    'source': 'hexbin-data',
+                    'layout': {},
+                    'paint': {
+                        'line-color': borderColor,
+                        'line-width': ['match', ['get', 'density'], 0, 0, 1]
+                    }
+                })
 
+                // individual crash data points
+                map.addLayer({
+                    id: 'points',
+                    type: 'circle',
+                    source: 'crash-data-source',
+                    paint: {
+                        'circle-color': pointColor,
+                        // adjust circle radius based on zoom level
+                        'circle-radius': ['interpolate', ['linear'], ['zoom'],
+                            // at zoom level 10 => 1 px
+                            10, 1,
+                            // at zoom level 12 => 1.5 px
+                            12, 1.5,
+                            14, 3,
+                            // at zoom level 20 => 20 px
+                            20, 20],
+                        // no stroke
+                        'circle-stroke-width': 0,
+                        'circle-opacity': 0.3
+                    }
+                })
+
+                /*
+                    POPUPS & HOVER EFFECTS
+                */
+                // Create a popup, but don't add it to the map yet.
+                const popup = new mapboxgl.Popup({
+                    closeButton: false,
+                    closeOnClick: false
+                })
+
+                // display the popup when a point is hovered over
                 map.on('mouseenter', 'points', (e) => {
                     // Change the cursor style as a UI indicator.
                     map.getCanvas().style.cursor = 'pointer';
@@ -194,41 +194,46 @@ function Map(props) {
                     // Populate the popup and set its coordinates
                     // based on the feature found.
                     popup.setLngLat(coordinates).setHTML(popupHTML).addTo(map);
-                });
+                })
 
+                // hide the popup when the point is no longer hovered
                 map.on('mouseleave', 'points', () => {
                     map.getCanvas().style.cursor = '';
                     popup.remove();
-                });
-
-                map.on('mousemove', 'districts', (e) => {
-                    console.log(e.features[0])
-
-                    if (e.features.length > 0) {
-                        if (hoveredStateId !== null) {
-                            map.setFeatureState(
-                                { source: 'council-districts', id: hoveredStateId },
-                                { hover: false }
-                            );
-                        }
-                        hoveredStateId = e.features[0].properties.district_id;
-                        map.setFeatureState(
-                            { source: 'council-districts', id: hoveredStateId },
-                            { hover: true }
-                        );
-                    }
                 })
+
+                // hover effect => council districts
+                let hoveredStateId = null;
+
+                // map.on('mousemove', 'districts', (e) => {
+                //     console.log(e.features[0])
+
+                //     if (e.features.length > 0) {
+                //         if (hoveredStateId !== null) {
+                //             map.setFeatureState(
+                //                 { source: 'council-districts', id: hoveredStateId },
+                //                 { hover: false }
+                //             );
+                //         }
+                //         hoveredStateId = e.features[0].properties.district_id;
+                //         map.setFeatureState(
+                //             { source: 'council-districts', id: hoveredStateId },
+                //             { hover: true }
+                //         );
+                //     }
+                // })
 
             })
         }
 
         if (!map) initializeMap({ setMap, mapContainer });
 
-    }, [data]);
+    }, [data, hexGridData]);
 
     useEffect(() => {
         if (map) {
             map.setLayoutProperty('hexBins', 'visibility', hexVisibility ? 'visible' : 'none');
+            map.setLayoutProperty('hex-borders', 'visibility', hexVisibility ? 'visible' : 'none');
         }
 
         console.log(map)
@@ -239,27 +244,11 @@ function Map(props) {
         if (map) {
             map.setLayoutProperty('districts', 'visibility', districtVisibility ? 'visible' : 'none');
             map.setLayoutProperty('district-labels', 'visibility', districtVisibility ? 'visible' : 'none');
+            map.setLayoutProperty('district-borders', 'visibility', districtVisibility ? 'visible' : 'none');
         }
+
+        console.log(map)
     }, [districtVisibility])
-
-    useEffect(() => {
-        if (map) {
-            map.setPaintProperty('hexBins', 'fill-opacity', [
-                "interpolate", ["linear"], ["get", "density"],
-                // if there are zero points, opacity = 0
-                0, 0,
-                // if the density = 1, opacity = 60%
-                1, hexOpacity * .2
-            ])
-        }
-    }, [hexOpacity])
-
-    // useEffect(() => {
-    //     if (map) {
-    //         const dotSizes = [2.5, 4, 8.5, 10]
-    //         map.setPaintProperty('points', 'circle-radius', dotSizes[parseInt(dotSize) - 1])
-    //     }
-    // }, [dotSize])
 
     return (
         <div ref={mapContainer} className="mapContainer" />
---
+++ src/MapContext.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react"
+import './App.css'
+import Map from './Map.js'
+import geojson from './data/master_crash_clean.geojson'
+import mapboxgl from 'mapbox-gl';
+import * as turf from '@turf/turf'
+
+function MapContext(props) {
+    const {
+        hexVisibility, districtVisibility
+    } = props
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY3RlcmJ1c2giLCJhIjoiY2t0dnZrYjM4MmU0aDJvbzM1dTFqbDY1NiJ9.zdZur9mZOlVhIxAoiqVwBA'
+
+    const [data, setData] = useState(geojson)
+    const [hexGridData, setHexGridData] = useState(null)
+
+    useEffect(() => {
+        const bbox = [-86.70764843085207, 38.980672784175255, -86.33708588689486, 39.54695628365925]
+        const cellSide = .5;
+        const options = {};
+        const hexagons = turf.hexGrid(bbox, cellSide, options);
+
+        hexagons.features.map((hex) => {
+            hex.properties.numPoints = turf.within(data, hex).features.length
+            return hex
+        })
+
+        const binWMostPoints = hexagons.features.reduce(
+            (prev, current) => {
+                return prev.properties.numPoints > current.properties.numPoints ? prev : current
+            }
+        )
+
+        hexagons.features.map((hex) => {
+            hex.properties.density = hex.properties.numPoints / binWMostPoints.properties.numPoints
+            return hex
+        })
+
+        setHexGridData(hexagons)
+    }, [data])
+
+    useEffect(() => {
+        fetch(data)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => setData(data));
+
+    }, [])
+
+    return (
+        <Map
+            hexVisibility={hexVisibility}
+            districtVisibility={districtVisibility}
+            data={data}
+            hexGridData={hexGridData}
+        />
+    )
+}
+
+export default MapContext;
