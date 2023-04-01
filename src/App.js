bb1cb3a17a8ba29bb190b194483ab9356bfbb4fe
carsonology <carson.terbush@gmail.com>
2023-04-01 17:11:19 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import './App.css';
-import Map from './Map.js'
+// import Map from './Map.js'
+import MapContext from './MapContext.js'
 import Controls from './Controls.js'
 
 function App() {
 
   const [hexVisibility, setHexVisibility] = useState(true)
-  const [hexOpacity, setHexOpacity] = useState(2)
-  const [dotSize, setDotSize] = useState(4)
   const [districtVisibility, setDistrictVisibility] = useState(true)
 
   return (
@@ -15,14 +14,10 @@ function App() {
       <Controls
         hexVisibility={hexVisibility}
         setHexVisibility={setHexVisibility}
-        hexOpacity={hexOpacity}
-        setHexOpacity={setHexOpacity}
-        dotSize={dotSize}
-        setDotSize={setDotSize}
         districtVisibility={districtVisibility}
         setDistrictVisibility={setDistrictVisibility}
       />
-      <Map hexVisibility={hexVisibility} hexOpacity={hexOpacity} dotSize={dotSize} districtVisibility={districtVisibility} />
+      <MapContext hexVisibility={hexVisibility} districtVisibility={districtVisibility} />
     </div>
   )
 }
